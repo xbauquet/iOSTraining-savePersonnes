a29d10f81bc8176bfec9556e2339a584e7514a4e
Xavier Bauquet <xavier.bauquet@gmail.com>
2016-01-27 14:58:11 +0100

    Initial Commit

---
+++ savePersonne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  savePersonne
+//
+//  Created by xavier on 27/01/2016.
+//  Copyright © 2016 xavier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ savePersonne/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  savePersonne
+//
+//  Created by xavier on 27/01/2016.
+//  Copyright © 2016 xavier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
